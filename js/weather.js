877e976baa1c70719acb1e2d863d62f976f6cc1c
Ann Cascarano <a.cascarano@gmail.com>
2016-02-15 13:53:04 -0500

    added Paris

--- js/weather.js
+++ js/weather.js
@@ -19,8 +19,10 @@ var cities = {
         "chicago" 		     : {coords: {latitude: 41.879003, longitude: -87.63675}},
         "san francisco"    : {coords: {latitude: 37.788531, longitude: -122.407237}},
         "austin"           : {coords: {latitude: 30.287532, longitude: -97.756348}},
+        "helsinki"         : {coords: {latitude: 60.169813, longitude: 24.93824}},
         "denver"           : {coords: {latitude: 39.7392, longitude: -104.9903}},
         "berlin"           : {coords: {latitude: -52.5167, longitude: 13.3833}},
+        "paris"           : {coords: {latitude: -48.8567, longitude: 2.3508}},
         "current location" : {coords: {latitude: 45.3833, longitude: -73.9833}} // Defaults to Montreal
        };
 
